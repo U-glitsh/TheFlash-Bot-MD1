4b52696010231f109beeed6cc202dd61843ba273
U-glitsh <itsmosscow@gmail.com>
2025-04-04 03:51:51 +0200

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
+    let message = '*<عشان تشوف الاوامر بتاعت البوت اكتب <.الاوامر*';
 await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
     conn.sendFile(m.chat, 'https://files.catbox.moe/wpahb6.jpg', 'image.jpg', message, m);
 };
